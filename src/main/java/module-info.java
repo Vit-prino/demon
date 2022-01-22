641ab0204083c9863e7086e1620ab6ca594504f7
Vit-prino <vitalii.pryndylas@privatbank.ua>
2022-01-22 23:17:53 +0200

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module com.example.demon {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.example.demon to javafx.fxml;
+    exports com.example.demon;
+}
